f25bd7c75d828c8182c1b0dce91bb57828b4bc44
Raissa <raissa.frota@capgemini.com>
2023-03-14 16:32:15 -0300

    Criando o Eureka Client

--- src/main/java/br/com/raissafrota/msclientes/MsclientesApplication.java
+++ src/main/java/br/com/raissafrota/msclientes/MsclientesApplication.java
@@ -2,8 +2,10 @@ package br.com.raissafrota.msclientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsclientesApplication {
 
 	public static void main(String[] args) {
